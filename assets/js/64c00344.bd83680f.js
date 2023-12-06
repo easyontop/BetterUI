e0426e2d93a6d6752225faad932e5236705a71e1
KLAE2160 <tunghinfungabel@gmail.com>
2023-12-06 20:51:15 +0800

    Built and published by Moonwave

---
+++ assets/js/64c00344.bd83680f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[426],{6213:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"SwitchReturnLibrary","desc":"A library stores the properties and functions of the switch\\n\\t\\t","source":{"line":24,"path":"src/BetterUI/Library/Switch.lua"}}')}}]);
